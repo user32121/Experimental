d09b2767cf209d862832c1fd7012b8db4c897ee5
user32121 <pengjupiter5@gmail.com>
2019-04-27 23:10:30 -0700

    Add project files.

---
+++ Fondere/Fondere.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Experimental
+{
+    partial class Fondere
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textSample = new System.Windows.Forms.TextBox();
+            this.buttonBuild = new System.Windows.Forms.Button();
+            this.textMaxChar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textRngRange = new System.Windows.Forms.TextBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.textPrevChar = new System.Windows.Forms.TextBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textSample
+            // 
+            this.textSample.Location = new System.Drawing.Point(13, 13);
+            this.textSample.Multiline = true;
+            this.textSample.Name = "textSample";
+            this.textSample.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textSample.Size = new System.Drawing.Size(300, 425);
+            this.textSample.TabIndex = 0;
+            this.textSample.Text = "tast satstsas";
+            // 
+            // buttonBuild
+            // 
+            this.buttonBuild.Location = new System.Drawing.Point(319, 12);
+            this.buttonBuild.Name = "buttonBuild";
+            this.buttonBuild.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuild.TabIndex = 1;
+            this.buttonBuild.Text = "rebuild";
+            this.buttonBuild.UseVisualStyleBackColor = true;
+            this.buttonBuild.Click += new System.EventHandler(this.ButtonBuild_Click);
+            // 
+            // textMaxChar
+            // 
+            this.textMaxChar.Location = new System.Drawing.Point(319, 41);
+            this.textMaxChar.Name = "textMaxChar";
+            this.textMaxChar.Size = new System.Drawing.Size(50, 20);
+            this.textMaxChar.TabIndex = 3;
+            this.textMaxChar.Text = "10";
+            this.textMaxChar.TextChanged += new System.EventHandler(this.TextMaxChar_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "maxCharacters\r\n\r\nrngRange\r\n\r\nprevCharacters";
+            // 
+            // textRngRange
+            // 
+            this.textRngRange.Location = new System.Drawing.Point(319, 67);
+            this.textRngRange.Name = "textRngRange";
+            this.textRngRange.Size = new System.Drawing.Size(50, 20);
+            this.textRngRange.TabIndex = 3;
+            this.textRngRange.Text = "3";
+            this.textRngRange.TextChanged += new System.EventHandler(this.TextRngRange_TextChanged);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.Location = new System.Drawing.Point(500, 13);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textOutput.Size = new System.Drawing.Size(288, 425);
+            this.textOutput.TabIndex = 10;
+            // 
+            // textPrevChar
+            // 
+            this.textPrevChar.Location = new System.Drawing.Point(319, 94);
+            this.textPrevChar.Name = "textPrevChar";
+            this.textPrevChar.Size = new System.Drawing.Size(50, 20);
+            this.textPrevChar.TabIndex = 3;
+            this.textPrevChar.Text = "2";
+            this.textPrevChar.TextChanged += new System.EventHandler(this.TextPrevChar_TextChanged);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(401, 13);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 2;
+            this.buttonGenerate.Text = "generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.ButtonGenerate_Click);
+            // 
+            // Fondere
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textMaxChar);
+            this.Controls.Add(this.buttonBuild);
+            this.Controls.Add(this.textSample);
+            this.Controls.Add(this.textRngRange);
+            this.Controls.Add(this.textPrevChar);
+            this.Name = "Fondere";
+            this.Text = "Fondere";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textSample;
+        private System.Windows.Forms.Button buttonBuild;
+        private System.Windows.Forms.TextBox textMaxChar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textRngRange;
+        private System.Windows.Forms.TextBox textOutput;
+        private System.Windows.Forms.TextBox textPrevChar;
+        private System.Windows.Forms.Button buttonGenerate;
+    }
+}
+
---
+++ Fondere/Fondere.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Experimental
+{
+    public partial class Fondere : Form
+    {
+        List<Node> trie = new List<Node>();
+        int maxChar;
+        int rngRange;
+        int prevChars;
+        Random rng = new Random();
+
+        public Fondere()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextMaxChar_TextChanged(this, EventArgs.Empty);
+            TextRngRange_TextChanged(this, EventArgs.Empty);
+            TextPrevChar_TextChanged(this, EventArgs.Empty);
+        }
+
+        private void ButtonBuild_Click(object sender, EventArgs e)
+        {
+            //refreshes the trie
+            trie.Clear();
+            trie.Add(new Node('a'));
+
+            //create node array
+            string sample = textSample.Text;
+            for (int i = 0; i < sample.Length; i++)
+                if (!char.IsLetter(sample[i]))
+                {
+                    sample = sample.Remove(i, 1);
+                    i--;
+                }
+            sample = sample.ToLower();
+
+            //iterates through each character and adds it to the trie
+            for (int i = 0; i < sample.Length - prevChars; i++)
+            {
+                int index = 0;
+                int prevIndex = -1;
+                //iterator for chain of characters
+                for (int j = 0; j <= prevChars; j++)
+                {
+                    bool done = false;
+                    //waits untill finished adding node
+                    while (!done)
+                    {
+                        prevIndex = index;
+                        if (trie[index].letter == sample[i + j])
+                        {
+                            //if correct node, adds 1 to uses for that node
+                            trie[index].uses++;
+                            done = true;
+                        }
+                        else
+                        {
+                            if (trie[prevIndex].nextNode == -1)
+                            {
+                                //if there is no nextNode, creates new node
+                                trie.Add(new Node(sample[i + j]) { uses = 1 });
+                                index = trie.Count - 1;
+                                trie[prevIndex].nextNode = index;
+                                done = true;
+                            }
+                            else
+                            {
+                                //if there is nextNode, moves index to nextNode
+                                index = trie[prevIndex].nextNode;
+                            }
+                        }
+                    }
+
+                    prevIndex = index;
+                    if (trie[prevIndex].daughterNode == -1)
+                    {
+                        //creates daughter node and moves index
+                        trie.Add(new Node('a'));
+                        index = trie.Count - 1;
+                        trie[prevIndex].daughterNode = index;
+                    }
+                    else
+                    {
+                        //moves index to daughterNode
+                        index = trie[prevIndex].daughterNode;
+                    }
+                }
+            }
+        }
+
+        private void TextMaxChar_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textMaxChar.Text, out int result) && result > 0)
+                maxChar = result;
+        }
+        private void TextRngRange_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textRngRange.Text, out int result) && result > 0)
+                rngRange = result;
+        }
+        private void TextPrevChar_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textPrevChar.Text, out int result) && result > 0)
+                prevChars = result;
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            textOutput.Text = "";
+
+            foreach (char start in new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' })
+            {
+                //generates random anti words
+                int index = 0;
+                string word = start.ToString();
+
+                while (word.Length < maxChar && index != -1)
+                {
+                    //prevCharCalculations
+                    for (int i = Math.Max(0, word.Length - prevChars); i < word.Length; i++)
+                    {
+                        //finds word in letter path
+                        while (!(trie[index].letter == word[i]))
+                            if (trie[index].nextNode != -1)
+                                index = trie[index].nextNode;
+                            else
+                                index = 0;
+
+                        index = trie[index].daughterNode;
+                    }
+
+                    //top letter occurences
+                    int[] topIndexes = new int[rngRange];
+
+                    for (int i = 0; i < rngRange; i++)
+                        topIndexes[i] = -1;
+
+                    //finds most used letters
+                    while (index != -1)
+                    {
+                        for (int i = 0; i < topIndexes.Length; i++)
+                            if (trie[index].uses > (topIndexes[i] == -1 ? -1 : trie[topIndexes[i]].uses))
+                            {
+                                topIndexes[i] = index;
+                                break;
+                            }
+
+                        index = trie[index].nextNode;
+                    }
+                    //counts the total number of valid values in topIndexes
+                    int totalPass = 0;
+                    for (int i = 0; i < topIndexes.Length; i++)
+                        if (topIndexes[i] != -1)
+                            totalPass++;
+
+                    word += trie[topIndexes[rng.Next(totalPass)]].letter;
+                    index = 0;
+
+                }
+
+                //outputs anti words
+                textOutput.Text += word + "\r\n";
+            }
+        }
+    }
+
+    class Node
+    {
+        public char letter;
+        public int nextNode = -1;
+        public int daughterNode = -1;
+        public int uses;
+
+        public Node(char id)
+        {
+            letter = id;
+        }
+    }
+}
